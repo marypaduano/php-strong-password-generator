4402216b150923e1ade520df774b48afcfe498b2
marypaduano <maria.paduano@gmail.com>
2023-03-21 17:34:47 +0100

    add input form

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>  
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header text-center">
+                       <h2>Password Generate</h2> 
+                    </div>
+                    <div class="card-body">
+                        <form method="GET" class="row">
+                            <div class="input-group d-flex justify-content-between">
+                                <h4>Lunghezza della password: </h4>
+                                <input type="text">
+                                <button class="btn btn-primary">Genera Password</button>                              
+                            </div>
+                        </form>
+                    <div>
+                </div>
+            </div>
+        </div>    
+    </div>
+</body>
+</html>
